39d2ebeda2716449eb7bb50b0ec5dc59372d3447
Flávia Jaconis <fla_njaconis@yahoo.com.br>
2022-03-02 10:21:27 -0300

    Exercicios curso Tecnico - 1 semestre

---
+++ 1º semestre/Desenvolvimento de Sistemas I/Agenda 03/Teste_Eclipse/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		import javax.swing.JOptionPane;
+		
+		public class JoptionPane {
+			public static void main(String args[]) {
+				string entrada; //variável de entrada
+				entrada = JOptionPane.showInputdialog(“Entre com um nome”);
+	}
+
+}
---
+++ 1º semestre/Desenvolvimento de Sistemas I/Agenda 15_DSI/automaticoSlideShow/javas.js
@@ -0,0 +1,4 @@
+var i;
+for (i = 0; i <= 100; i++) {
+ document.write(i + " <br>");
+}
